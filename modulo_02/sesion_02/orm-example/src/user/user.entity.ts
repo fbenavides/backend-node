5710a15bb8453c785258f12d23a0772fedd0f7ad
Fernando Benavides <fbenavides1991@gmail.com>
2026-04-13 20:43:46 -0500

    modulo02_sesion02

---
+++ modulo_02/sesion_02/orm-example/src/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Order } from '../order/order.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @OneToMany(() => Order, (order) => order.user)
+  // @OneToMany(() => Order, (order) => order.user, { eager: true })
+  // orders!: Order[];
+  orders!: Promise<Order[]>;
+}
---
+++ modulo_02/sesion_02/pasos.md
@@ -0,0 +1,20 @@
+# ORM
+
+## Levantar nuestra db en docker
+
+```
+docker compose up -d
+```
+
+
+## Crear proyecto nest
+
+```
+nest new orm-example
+```
+
+## Instalar dependencias
+
+```
+pnpm add mysql2 @nestjs/config typeorm @nestjs/typeorm class-validator class-transformer
+```
